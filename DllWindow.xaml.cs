7d81efa67769b643fd8aa42b39d23dd7fb58798e
NikitaB1221 <44338356+NikitaB1221@users.noreply.github.com>
2023-04-01 23:03:45 +0300

    Final
    У Секундомера в окне TimerWindow иконка от Freepik - Flaticon( Это требование для бесплатного использования )

--- DllWindow.xaml.cs
+++ DllWindow.xaml.cs
@@ -17,11 +17,12 @@ using static SystemProgramming_111.MultiWindow;
 
 namespace SystemProgramming_111
 {
-    /// <summary>
-    /// Логика взаимодействия для DllWindow.xaml
-    /// </summary>
     public partial class DllWindow : Window
     {
+        #region Basics
+        /// <summary>
+        /// Логика взаимодействия для DllWindow.xaml
+        /// </summary>
         [DllImport("User32.dll")]
         public static extern
             int MessageBoxA(
@@ -40,22 +41,108 @@ namespace SystemProgramming_111
             );
         [DllImport("Kernel32.dll", EntryPoint = "Beep")]
         public static extern bool Sound(uint dwFreq, uint dwDuration);
+
+        [DllImport("Kernel32.dll", EntryPoint = "CreateThread")]
+        public static extern IntPtr CreateThread(
+            IntPtr lpThreadAttributes,
+            uint dwStackSize,
+            ThreadMethod lpStartAddress,
+            IntPtr lpParameter,
+            uint dwCreationFlags,
+            IntPtr lpThreadId
+            );
+
+        public delegate void ThreadMethod();
+
+        public void SayHello()
+        {
+            Dispatcher.Invoke(() => SayHelloLabel.Content = "Hello");
+            sayHelloHandle.Free();
+        }
+        #endregion
+
+        #region MM Timer  
+
+        delegate void TimerMethod(uint uTimer, uint uMsg, ref uint dwUser, uint dw1, uint dw2);
+        [DllImport("winmm.dll", EntryPoint = "timeSetEvent")]
+        static extern uint timeSetEvent(
+            uint Delay,
+            uint uResolution,
+            TimerMethod lpTimeProc,
+            ref uint dwUser,
+            uint eventType
+            );
+
+        [DllImport("winmm.dll", EntryPoint = "timeKillEvent")]
+        static extern uint timeKillEvent(uint uTimerId);
+
+        const uint TIME_ONESHOT = 0;
+        const uint TIME_PERIODIC = 1;
+
+        uint uDelay;
+        uint uResolution;
+        uint timerId;
+        uint dwUser = 0;
+        TimerMethod timerMethod = null!;
+        GCHandle timerHandle;
+
+        int ticks;
+        void TimerTick(uint uTimer, uint uMsg, ref uint dwUser, uint dw1, uint dw2)
+        {
+            ticks++;
+            Dispatcher.Invoke(() => {
+                TicksLabel.Content = ticks.ToString();
+            });
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            uDelay = 100;       //  Задержка между вызовами 10 ms = 10 Hz
+            uResolution = 10;   //  Допустимая погрешность
+            ticks = 0;
+            timerMethod = new TimerMethod(TimerTick);
+            timerHandle = GCHandle.Alloc(timerMethod);
+            timerId = timeSetEvent(uDelay, uResolution, timerMethod, ref dwUser, TIME_PERIODIC);
+            if (timerId != 0)
+            {
+                Start.IsEnabled = false;
+                Stop.IsEnabled = true;
+            }
+            else
+            {
+                timerHandle.Free();
+                timerMethod = null!;
+            }
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            timeKillEvent(timerId);
+            timerHandle.Free();
+            Start.IsEnabled = true;
+            Stop.IsEnabled = false;
+        }
+
+        #endregion
+
         public DllWindow()
         {
             InitializeComponent();
         }
+
+        #region Basics methods
         private void MsgA_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxA(IntPtr.Zero,"Message","Title",1);
+            MessageBoxA(IntPtr.Zero, "Message", "Title", 1);
         }
         private void MsgW_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxW(IntPtr.Zero,"Message","Title",1);
+            MessageBoxW(IntPtr.Zero, "Message", "Title", 1);
 
         }
         private void Msg1_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxA(IntPtr.Zero,"Повторить попытку"
+            MessageBoxA(IntPtr.Zero, "Повторить попытку"
                 , "Соединение не установленно", 0x35);
         }
 
@@ -73,12 +160,12 @@ namespace SystemProgramming_111
         private bool? ConfirmMessage(String message)
         {
             int res = MessageBoxW(IntPtr.Zero, message, "", 0x46);
-                return res switch
-                {
-                    11 => true,   //Continue
-                    10 => false,  //Try again
-                    _ => null     //Cancel (res = 2)
-                };
+            return res switch
+            {
+                11 => true,   //Continue
+                10 => false,  //Try again
+                _ => null     //Cancel (res = 2)
+            };
 
         }
 
@@ -120,22 +207,22 @@ namespace SystemProgramming_111
 
         private void RedXylophoneKey_Click(object sender, RoutedEventArgs e)
         {
-            SMaker(250 , 250);
+            SMaker(250, 250);
         }
 
         private void YellowXylophoneKey_Click(object sender, RoutedEventArgs e)
         {
-            SMaker(500 , 250);
+            SMaker(500, 250);
         }
 
         private void GreenXylophoneKey_Click(object sender, RoutedEventArgs e)
         {
-            SMaker(750 , 250);
+            SMaker(750, 250);
         }
 
         private void BlueXylophoneKey_Click(object sender, RoutedEventArgs e)
         {
-            SMaker(1000 , 250);
+            SMaker(1000, 250);
         }
 
 
@@ -153,5 +240,34 @@ namespace SystemProgramming_111
                 Monitor.Exit(monitor);  // Выход == разблокирование
             }
         }
+
+        GCHandle sayHelloHandle;
+        private void Thread_Click(object sender, RoutedEventArgs e)
+        {
+            // CreateThread(IntPtr.Zero, 0, SayHello, IntPtr.Zero, 0, IntPtr.Zero);
+            // Потенциальная проблема - сборщик мусора. При работе он дефрагментирует
+            //  память, перенося объекты между поколениями
+            // [.][..][.][.x.][..][.] ==> [.][..][.]     [..][.] ==> [.][..][.][..][.]
+            //                                                                 эти два
+            // объекта поменяют свой адрес в памяти
+            // Необходимо "сказать" сборщику мусора о том, что объект не нужно перемещать
+            // Для того чтобы не "фиксировать" целое окно, отделим метод в новый объект
+            var sayHelloObject = new ThreadMethod(SayHello);
+            // и укажем сборщику мусора (GC) разместить этот объект на постоянном месте
+            sayHelloHandle = GCHandle.Alloc(sayHelloObject);
+            // передаем в неуправляемый код ссылку на объект sayHelloObject
+            CreateThread(IntPtr.Zero, 0, sayHelloObject, IntPtr.Zero, 0, IntPtr.Zero);
+            // долго удерживать объекты на одном месте нежелательно, после использования
+            // нужно их "расфиксировать" - см. SayHello()
+        }
+
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new TimerWindow().Show();  //  По задумке можно сделать несколько Таймеров
+        }
     }
+
+
 }
---
+++ TimerWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SystemProgramming_111
+{
+    /// <summary>
+    /// Логика взаимодействия для TimerWindow.xaml
+    /// </summary>
+    public partial class TimerWindow : Window
+    {
+        #region MM Timer  
+
+        delegate void TimerMethod(uint uTimer, uint uMsg, ref uint dwUser, uint dw1, uint dw2);
+        [DllImport("winmm.dll", EntryPoint = "timeSetEvent")]
+        static extern uint timeSetEvent(
+            uint Delay,
+            uint uResolution,
+            TimerMethod lpTimeProc,
+            ref uint dwUser,
+            uint eventType
+            );
+
+        [DllImport("winmm.dll", EntryPoint = "timeKillEvent")]
+        static extern uint timeKillEvent(uint uTimerId);
+
+        const uint TIME_ONESHOT = 0;
+        const uint TIME_PERIODIC = 1;
+
+        uint uDelay;
+        uint uResolution;
+        uint timerId;
+        uint dwUser = 0;
+        TimerMethod timerMethod = null!;
+        GCHandle timerHandle;
+
+        uint HH = 0;
+        uint MM = 0;
+        uint SS = 0;
+        uint DS = 0;
+
+        void TimerTick(uint uTimer, uint uMsg, ref uint dwUser, uint dw1, uint dw2)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                DecesecondLabel.Content = (DS <= 9 ? "0" + DS : DS);
+                SecondLabel.Content = (SS <= 9 ? "0" + SS : SS);
+                MinuteLabel.Content = (MM <= 9 ? "0" + MM : MM);
+                HoursLabel.Content = (HH <= 9 ? "0" + HH : HH);
+            });
+            DS++;
+            if (DS == 10)
+            {
+                DS = 0;
+                SS++;
+            }
+            if (SS == 60)
+            {
+                SS = 0;
+                MM++;
+            }
+            if (MM == 60)
+            {
+                MM = 0;
+                HH++;
+            }
+        }
+
+        #endregion
+
+        public TimerWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            uDelay = 100;      
+            uResolution = 0;   
+            timerMethod = new TimerMethod(TimerTick);
+            timerHandle = GCHandle.Alloc(timerMethod);
+            timerId = timeSetEvent(uDelay, uResolution, timerMethod, ref dwUser, TIME_PERIODIC);
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            timeKillEvent(timerId);
+            timerHandle.Free();
+            timerMethod = null!;
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (timerHandle.IsAllocated)
+            {
+                timeKillEvent(timerId);
+                timerHandle.Free();
+                timerMethod = null!;
+            }
+            HH = 0;
+            MM = 0;
+            SS = 0;
+            DS = 0;
+            Dispatcher.Invoke(() =>
+            {
+                DecesecondLabel.Content = "00";
+                SecondLabel.Content = "00";
+                MinuteLabel.Content = "00";
+                HoursLabel.Content = "00";
+            });
+        }
+    }
+}
